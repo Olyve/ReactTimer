28ba89acc86f7de77ad1f8ebfdda94a9ab48104a
Sam Galizia <samgalizia@gmail.com>
2017-05-02 18:10:43 -0400

    Initial commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,23 @@
+let React = require('react');
+let {Link, IndexLink} = require('react-router');
+
+let Nav = () => {
+  return (
+    <div className='top-bar'>
+      <div className='top-bar-left'>
+        <ul className='menu'>
+          <li className='menu-text'>React Timer App</li>
+          <li><IndexLink to='/' activeClassName='active-link' activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink></li>
+          <li><Link to='/' activeClassName='active-link' activeStyle={{fontWeight: 'bold'}}>Countdown</Link></li>
+        </ul>
+      </div>
+      <div className='top-bar-right'>
+        <ul className='menu'>
+          <li className='menu-text'>Created by <a href='https://www.github.com/sgalizia' target='_blank'>Sam Galizia</a></li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+module.exports = Nav;
